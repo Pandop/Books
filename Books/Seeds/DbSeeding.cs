078b788e1b4a6a0ab57e646741ba9b162add2972
kel01 <s.kel@student.qut.edu.au>
2019-09-27 16:43:18 +1000

    Add Db seeding class

---
+++ Books/Seeds/DbSeeding.cs
@@ -0,0 +1,21 @@
+﻿using Books.Models;
+using Books.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Seeds
+{
+	public static class DbSeeding
+	{
+		public static void SeedDataContext(this BookDbContext bookContext)
+		{
+			// extension method hence this
+			var bookAuthors = new List<BookAuthor>()
+			{
+				// new BookAuthor
+			};
+		}
+	}
+}
