563df64e16171a06b985f4cd998f0fe4c544ca1e
kel01 <s.kel@student.qut.edu.au>
2019-09-27 16:09:43 +1000

    Added many to many relationship

---
+++ Books/Services/BookDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Books.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Services
+{
+	public class BookDbContext: DbContext
+	{
+		public BookDbContext(DbContextOptions<BookDbContext> options): base(options)
+		{
+			// run migration when app starts up
+			Database.Migrate();
+		}
+
+		// Create DBSet to tell EF what tables we need to setup
+		public virtual DbSet<Book>	Books { get; set; }
+		public virtual DbSet<Author> Authors { get; set; }
+		public virtual DbSet<Review> Reviews { get; set; }
+		public virtual DbSet<Reviewer> Reviewers { get; set; }
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Country> Countries { get; set; }
+		public virtual DbSet<BookAuthor> BookAuthors { get; set; }
+		public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+		// Setup many to many relationships
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// many to many relationships i.e. Books and Authors
+			modelBuilder.Entity<BookAuthor>()
+						.HasKey(ba => new { ba.BookId, ba.AuthorId });
+			modelBuilder.Entity<BookAuthor>()
+						.HasOne(a => a.Book)
+						.WithMany(ba => ba.BookAuthors)
+						.HasForeignKey(a => a.BookId);
+			modelBuilder.Entity<BookAuthor>()
+						.HasOne(b => b.Book)
+						.WithMany(ba => ba.BookAuthors)
+						.HasForeignKey(b => b.BookId);
+
+			// many to many relationships i.e. Books and Categories
+			modelBuilder.Entity<BookCategory>()
+						.HasKey(bc => new { bc.BookId, bc.CategoryId });
+			modelBuilder.Entity<BookCategory>()
+						.HasOne(b => b.Book)
+						.WithMany(bc => bc.BookCategories)
+						.HasForeignKey(b => b.BookId);
+			modelBuilder.Entity<BookCategory>()
+						.HasOne(c => c.Category)
+						.WithMany(bc => bc.BookCategories)
+						.HasForeignKey(c => c.CategoryId);
+
+		}
+
+	}
+}
--- Books/Startup.cs
+++ Books/Startup.cs
@@ -5,16 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Books
 {
 	public class Startup
 	{
+		public static IConfiguration Configuration { get; set; }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			var connectionString = Configuration["connectionStrings:bookDbConnectionString"]; //services.AddC();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
